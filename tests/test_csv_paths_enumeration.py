39340a3e6f3e6032075e0d498f41168be6d97320
fjobs <florianjobs@gmx.de>
2026-07-23 14:57:44 +0200

    Added testing for csv-path discovery.

--- tests/test_csv_paths_enumeration.py
+++ tests/test_csv_paths_enumeration.py
@@ -7,6 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description="Run COCOA indexing.")
     parser.add_argument("--corpora", required=False,
                         help="Directory containing the table corpora. Defaults to dataset/.")
+    parser.add_argument("--limit", required=False, help="Only process the first n csv files.")
     args = parser.parse_args()
 
     # Obtain all csv paths. If --corpora is specified, use that, else use dataset/. Possible error source: empty csv's.
