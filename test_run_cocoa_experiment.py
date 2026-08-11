986404a2b8d3a653c5df40a3ef824a766f621a3a
florian_jobs <florian.jobs@campus.tu-berlin.de>
2026-08-11 11:09:38 +0200

    minor

--- test_run_cocoa_experiment.py
+++ test_run_cocoa_experiment.py
@@ -15,7 +15,7 @@ import pandas as pd
 def main():
     parser = argparse.ArgumentParser("Run Cocoa Baseline Test")
     parser.add_argument("--corpora", required=True, help="Corpora to run")
-    parser.add_argument("--limit", required=True, help="Number of csv's to process")
+    parser.add_argument("--limit", required=False, help="Number of csv's to process")
     parser.add_argument("--input", required=True, help="CSV file to run the experiment on, e.g. dataset/movie.csv")
     parser.add_argument("--query_column", required=True, help="Query column to run the experiment on")
     parser.add_argument("--target_column", required=True, help="Target column to run the experiment on")
